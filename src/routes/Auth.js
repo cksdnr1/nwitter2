f2e2c8c001feed6882a49a8701833835f2e86004
unknown <cksdnr1@gmail.com>
2021-06-17 00:59:49 +0900

    r1.4 0617_lecture

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,11 @@
 import React, {useState} from "react";
+import {authService} from 'myFB';
 
 const Auth = () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	const [newAccount, setNewAccount] = useState(true);
+	const [error, setError] = useState("");
 	const onChange = (event) => {
 		const {target : {name,value}} = event;
 		if (name === "email"){
@@ -11,16 +14,30 @@ const Auth = () => {
 			setPassword(value);
 		}
 	};
-	const onSubmit = (event) => {
+	const onSubmit = async(event) => {
 		event.preventDefault();
+		try{
+			let data;
+			if(newAccount){
+				data = await authService.createUserWithEmailAndPassword(email, password);
+
+			} else {
+				data = await authService.signInWithEmailAndPassword(email, password);
+			}
+		} catch(error){
+			setError(error.message)
+		}
 	};
+	const toggleAccount = () => setNewAccount(prev => !prev);
 	return (
 	<div>
 		<form onSubmit={onSubmit}>
 			<input name="email" type='email' placeholder="Email" required value={email} onChange={onChange} />
 			 <input name="password" type='password' placeholder="Password" required={password} onChange={onChange}/>
-			<input type='submit' value="Log In" />
+			<input type='submit' value={newAccount ? "Create Account" : "Log In"} />
+		{error}
 		</form>
+		<span onClick={toggleAccount}>{newAccount ? "Sign in":"Create Account"} </span>
 		<div>
 			<button>Continue with Google</button>
 			<button>Continue with Github</button>
