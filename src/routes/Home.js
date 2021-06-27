4ea0ea097e274f241dbffd6490b674e32876eb38
LEECHANWOOK <cksdnr1@gmail.com>
2021-06-28 00:04:09 +0900

    done

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -7,19 +7,22 @@ const Home = ({ userObj }) => {
 	const [nweets, setNweets] = useState([]);
 
 	useEffect(() => {
-		dbService.collection('nweets').onSnapshot((snapshot) => {
-			const nweetArray = snapshot.docs.map((doc) => ({
-				id: doc.id,
-				...doc.data(),
-			}));
-			setNweets(nweetArray);
-		});
+		dbService
+			.collection('nweets')
+			.orderby('createdAt', 'desc')
+			.onSnapshot((snapshot) => {
+				const nweetArray = snapshot.docs.map((doc) => ({
+					id: doc.id,
+					...doc.data(),
+				}));
+				setNweets(nweetArray);
+			});
 	}, []);
 
 	return (
-		<div className = "container">
-			<NweetFactory userObj={userObj}/>
-			<div style ={{marginTop : 30}}>
+		<div className="container">
+			<NweetFactory userObj={userObj} />
+			<div style={{ marginTop: 30 }}>
 				{nweets.map((nweet) => (
 					<Nweet
 						key={nweet.id}
